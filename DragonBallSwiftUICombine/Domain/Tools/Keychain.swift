205315294d8cead1b9da462512d08444a38e1898
Roberto Rojo <rrojo.va@gmail.com>
2022-11-02 13:32:06 +0100

    Create Keychain class to use KeychainSwift Package

---
+++ DragonBallSwiftUICombine/Domain/Tools/Keychain.swift
@@ -0,0 +1,36 @@
+//
+//  Keychain.swift
+//  DragonBallSwiftUICombine
+//
+//  Created by Roberto Rojo Sahuquillo on 2/11/22.
+//
+
+import KeychainSwift
+
+final class Keychain {
+    var keychain = KeychainSwift()
+    
+    //Function to save keychain
+    func saveKeychain(key: String, value: String) -> Bool {
+        if let data = value.data(using: .utf8){
+            keychain.set(data, forKey: key)
+            return true
+        } else{
+            return false
+        }
+    }
+    
+    //Function to read keychain
+    func readKeychain(key: String) -> String? {
+        if let data = keychain.get(key){
+            return data
+        } else{
+            return ""
+        }
+    }
+    
+    //Function to delete keychain
+    func deleteKeychain(key: String) {
+        keychain.delete(key)
+    }
+}
