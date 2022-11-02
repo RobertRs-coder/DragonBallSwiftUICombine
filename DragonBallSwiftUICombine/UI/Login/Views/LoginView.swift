4f88495783eaeda0ee2adb453f05d64a7a7486da
Roberto Rojo <rrojo.va@gmail.com>
2022-11-02 13:57:00 +0100

    Login Functionality, first step LoginView

---
+++ DragonBallSwiftUICombine/UI/Login/Views/LoginView.swift
@@ -0,0 +1,22 @@
+//
+//  LoginView.swift
+//  DragonBallSwiftUICombine
+//
+//  Created by Roberto Rojo Sahuquillo on 2/11/22.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        ZStack{
+            Image(decorative: "backgroundLogin")
+        }
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
