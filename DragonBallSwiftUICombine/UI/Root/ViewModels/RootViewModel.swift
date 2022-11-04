73f215a3760d43558064c359f51baa6a146f47fd
Roberto Rojo <rrojo.va@gmail.com>
2022-11-04 14:24:05 +0100

    Create function login to call the server inside RootViewModel

--- DragonBallSwiftUICombine/UI/Root/ViewModels/RootViewModel.swift
+++ DragonBallSwiftUICombine/UI/Root/ViewModels/RootViewModel.swift
@@ -15,5 +15,21 @@ final class RootViewmodel: ObservableObject{
     
     private var subscribers = Set<AnyCancellable>()
     
+    //Login to server
+    func login(user: String, password: String) {
+      
+        self.status = .loading
         
+        URLSession.shared
+            .dataTaskPublisher(for: BaseNetwork().getSessionLogin(user: user, password: password))
+            .tryMap {
+                guard let response = $0.response as? HTTPURLResponse,
+                      response.statusCode == 200 else {
+                    throw URLError(.badServerResponse)
+                }
+                //Return the string because it's the token
+                return String(decoding: $0.data, as: UTF8.self)
+            }
+    }
+
 }
