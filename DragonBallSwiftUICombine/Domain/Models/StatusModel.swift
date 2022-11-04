0dae098e5c4b3dc460e84a461262321977f6b59b
Roberto Rojo <rrojo.va@gmail.com>
2022-11-04 13:50:29 +0100

    Create StatusModel & RootViewModel

---
+++ DragonBallSwiftUICombine/Domain/Models/StatusModel.swift
@@ -0,0 +1,12 @@
+//
+//  StatusModel.swift
+//  DragonBallSwiftUICombine
+//
+//  Created by Roberto Rojo Sahuquillo on 4/11/22.
+//
+
+import Foundation
+
+enum Status {
+    case none, loading, loaded, register, error(error: String)
+}
---
+++ DragonBallSwiftUICombine/UI/Root/ViewModels/RootViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewModel.swift
+//  DragonBallSwiftUICombine
+//
+//  Created by Roberto Rojo Sahuquillo on 4/11/22.
+//
+
+import Foundation
+import Combine
+
+final class RootViewmodel: ObservableObject{
+    //MARK: Published variables
+    @Published var status = Status.none
+    @Published var tokenJWT: String = ""
+    
+    private var subscribers = Set<AnyCancellable>()
+    
+    
+    
+}
